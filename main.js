a60f5945a9e5c13249ba0edd9b8d2888d2fa2e1d
Bigeard <robin.bigeard@gmail.com>
2018-03-15 22:49:13 +0100

    petite avancer sur le canvas

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ function pageLoaded() {
   //sound.src = "music/Eterna_City.mp3";
   //sound.play();
 
-  var canvas = document.getElementById("message");
-  var context = canvas.getContext("2d");
-  //--------------------------------------------------
-  context.font = "10 pt Arial";
-  context.fillText("This is in 10 pt Arial. . .", 0, 0,300,150);
-  //--------------------------------------------------
-  context.fillStyle = "red";
-  context.fillRect(0, 0, 300, 150);//300x150 est taille par défaut d'un canvas (sur chrome en tout cas), donc ça représente un 100%, 100% du message :/
+	var canvas = document.getElementById('message');
+	var context = canvas.getContext('2d');
+
+	context.font = '8pt PS2P';
+	context.fillStyle = '#fff';
+	context.fillText(messageTest.content[0], 10, 25);
+  context.fillText(messageTest.content[1], 10, 45);
+  context.fillText(messageTest.content[2], 10, 65);
 }
